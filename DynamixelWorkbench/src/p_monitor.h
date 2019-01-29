cbf11b6974a7caab43fa5d3ce9274303d6ec5124
Étienne Villemure <vile2904@usherbrooke.ca>
2019-01-29 11:18:36 -0500

    Rajout d'un main du openCr et mettre à l'externe la librarie de pmonitor

--- DynamixelWorkbench/src/p_monitor.h
+++
@@ -1,2 +0,0 @@
-#include "../include/dynamixel_workbench_toolbox/p_monitor.h"
-
---
+++ main_open_cr/main_open_cr.ino
@@ -0,0 +1,35 @@
+
+#include <DynamixelWorkbench.h>
+#include "./p_monitor/p_monitor.h"
+
+#if defined(__OPENCM904__)
+  #define DEVICE_NAME "3" //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
+#elif defined(__OPENCR__)
+  #define DEVICE_NAME ""
+#endif
+
+String cmd[64];
+
+void setup()
+{
+    // Initialisation
+    Serial.begin(9600);
+    while(!Serial);     // Wait until the serial is ready
+    
+    cmd[0]="help";
+}
+
+void loop() 
+{
+    if(Serial.available()>0)
+    {
+      String read_string = Serial.readStringUntil('\n');
+      Serial.println("[CMD] : " + String(read_string));
+      
+      read_string.trim();
+
+      split(read_string, ' ', cmd);
+
+      dynamixel_command(cmd);
+    }
+}
