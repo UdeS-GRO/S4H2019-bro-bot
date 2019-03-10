af4766afe80eb7de95e7a7f59b246305b195899d
Étienne Villemure <vile2904@usherbrooke.ca>
2019-03-10 12:49:18 -0400

    remove problematic lines

--- pi_interface/automated_test/interface_pi.py
+++ pi_interface/automated_test/interface_pi.py
@@ -3,10 +3,9 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-if not __name__ == "__main__":
-
 
 from threading import Thread,Condition,RLock
+
 from tkinter import Tk, Button, Label, Entry, W, E, Checkbutton, BooleanVar, END, INSERT, Text
 import time
 import serial
